cf4e91d1ea338242f513a36e54fa8147206240f4
adastraz <tylergosox@gmail.com>
2019-12-11 13:32:45 -0500

    normal mvp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import axios from 'axios'
 import "./App.css";
 
 function App() {
+
+  const [photo, setPhoto] = useState("")
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=34a6Fg3WFnhFpRJNICb3XfUx77gYHBXUO0hy9f2A&date=2018-03-28')
+      .then(response => {
+        console.log(response.data.url)
+        setPhoto(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },[])
 
   return (
     <div className="App">
      <>
      <div className='photoHolder'>
-       this is a div
+       <img src = {photo.url} alt={photo.title}/>
      </div>
      </>
     </div>
